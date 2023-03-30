f3287f105fd7665fdf5701bc140dff1b83886d48
grimripper <inggyovannycavazos@gmail.com>
2023-03-30 16:41:51 -0700

    chat gpt tutorial part3

--- 03website.py
+++ 03website.py
@@ -1,4 +1,21 @@
 import openai
 import gradio
 
-openai.api_key=''
+openai.api_key = ''
+
+messages = [{'role': 'system', 'content': 'You are a psychologist'}]
+
+
+def CustomChatGPT(user_input):
+    messages.append({'role': 'user', 'content': user_input})
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+    ChatGPT_reply = response['choices'][0]['message']['content']
+    messages.append({'role': 'assistant', 'content': ChatGPT_reply})
+    return ChatGPT_reply
+
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs='text', outputs='text', title='Your Title')
+demo.launch()
