c16a984cc702f732503d30c2fc25059c494b6495
grimripper <inggyovannycavazos@gmail.com>
2023-03-30 15:59:45 -0700

    ready

--- 01Simple.py
+++ 01Simple.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = ""
 
 completion = openai.ChatCompletion.create(model ="gpt-3.5-turbo", messages=[{'role':'user', 'content':'can you draw a penguin in ascii'}])
 print(completion.choices[0].message.content)
